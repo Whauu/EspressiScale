c7d2c02e34200ae6d295a9a6a3b5b11393430929
Whauu <hasse.gudmestad@hotmail.com>
2025-07-19 22:20:26 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -398,6 +398,11 @@ void loop()
       timer_running = !timer_running; // Toggle timer state
       delay(1000); // Debounce delay
     }
+    else if (x < screenWidth / 2 && x > screenWidth / 2)
+    {
+      Serial.println("Touch registered on both sides, entering deep sleep...");
+      deep_sleep(); // Enter deep sleep if both sides are touched
+    }
     else
     {
       timer_running = false; // Stop the timer
