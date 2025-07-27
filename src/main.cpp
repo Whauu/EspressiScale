3d59928b60861dd1c56033358d59b84466128545
Hasse <120029840+Whauu@users.noreply.github.com>
2025-07-27 22:49:55 +0200

    Revert change main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -396,7 +396,6 @@ void loop()
     if (x > screenWidth / 2)
     {
       timer_running = !timer_running; // Toggle timer state
-      x = 0;
       delay(1000); // Debounce delay
       if (x > screenWidth / 2)
       {
