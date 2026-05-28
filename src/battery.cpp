862b149d8dd14bbf60e5f50cc364029ab6193e3a
Whauu <hasse.gudmestad@hotmail.com>
2026-05-28 13:09:45 +0200

    Update battery.cpp

--- src/battery.cpp
+++ src/battery.cpp
@@ -14,22 +14,11 @@ BQ25896  bq(Wire);
 
 void setupBattery(){
     Wire.begin(I2C_SDA, I2C_SCL);
-    Wire.setClock(100000);
-
-    Serial.println("Reading BQ registers...");
-
-    uint8_t reg03 = readBQ(0x03); // SYS_CTRL
-    uint8_t reg0B = readBQ(0x0B); // VBUS_STAT
-    uint8_t reg0C = readBQ(0x0C); // FAULT
-    uint8_t reg0E = readBQ(0x0E); // BATV
-    uint8_t reg14 = readBQ(0x14); // CTRL2 
-
-    Serial.print("REG03 SYS_CTRL = 0x"); Serial.println(reg03, HEX);
-    Serial.print("REG0B STATUS   = 0x"); Serial.println(reg0B, HEX);
-    Serial.print("REG0C FAULT    = 0x"); Serial.println(reg0C, HEX);
-    Serial.print("REG0E BATV     = 0x"); Serial.println(reg0E, HEX);
-    Serial.print("REG14 CTRL2    = 0x"); Serial.println(reg14, HEX);
-    /*Wire.begin();
+    if (!checkBQ()) {
+        Serial.println("BQ25896 not detected. Check wiring.");
+        delay(2000);
+    }
+    Serial.println("BQ25896 detected successfully.");
     bq.begin();
     bq.setBatLoad(ENABLED);
     pinMode(OTG_PIN, OUTPUT); // Pin 2 controls power to the battery circuit
@@ -37,7 +26,7 @@ void setupBattery(){
     delay(100); // Wait for OTG pin set high to stabilize
     bq.setOTGEnable(ENABLED); // Enable OTG power for the battery circuit
     delay(100); // Wait for the battery circuit to stabilize
-    printBQStatus();*/
+    printBQStatus();
 }
 
 float getBatteryVoltage(){
@@ -93,23 +82,8 @@ void printBQStatus()
             break;
     }
 }
-uint8_t readBQ(uint8_t reg)
+bool checkBQ()
 {
-    Wire.beginTransmission(BQ_ADDR);
-    Wire.write(reg);
-
-    uint8_t err = Wire.endTransmission(false); // repeated start
-    if (err != 0) {
-        Serial.print("Write reg failed, err=");
-        Serial.println(err);
-        return 0xFF;
-    }
-
-    uint8_t n = Wire.requestFrom(BQ_ADDR, (uint8_t)1, (uint8_t)true);
-    if (n != 1) {
-        Serial.println("Read failed");
-        return 0xFF;
-    }
-
-    return Wire.read();
+    Wire.beginTransmission(0x6A);
+    return Wire.endTransmission() == 0;
 }
